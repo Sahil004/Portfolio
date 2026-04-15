495fa88dc75a176146c217ca38032129471f8b01
Sahil004 <parvezsayyad0045@gmail.com>
2026-04-15 15:45:00 +0530

    added create user

--- portfolio-backend/backend/projects/apps.py
+++ portfolio-backend/backend/projects/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+import os
 
 class ProjectsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'projects'
+
+    def ready(self):
+        if os.environ.get("CREATE_SUPERUSER") == "True":
+            User = get_user_model()
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@example.com",
+                    password="admin12345"
+                )
